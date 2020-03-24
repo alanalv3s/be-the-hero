b5df873dc0a20e2c48e1692ddfecb7ed39a87f69
alanams97 <alan_ams97@gmail.com>
2020-03-24 17:57:05 -0300

    Configurar Knex e configurar migrations.

---
+++ backend/src/database/migrations/20200324172517_create_ongs.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('ongs', function(table) {
+      table.string('id').primary();
+      table.string('name').notNullabe();
+      table.string('email').notNullabe();
+      table.string('whatsapp').notNullabe();
+      table.string('city').notNullabe();
+      table.string('uf', 2).notNullabe();
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('ongs')
+};
+
