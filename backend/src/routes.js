e76d267899f7cc368784fac738acc7e1a48c1462
alanams97 <alan_ams97@gmail.com>
2020-03-24 18:58:34 -0300

    Correção em json(ongs)

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -7,7 +7,7 @@ const routes = express.Router();
 routes.get('/ongs', async(request, response) => {
     const ongs = await connection('ongs').select('*');
 
-    return response.json('ongs');
+    return response.json(ongs);
 })
 
 routes.post('/ongs', async (request, response) => {
