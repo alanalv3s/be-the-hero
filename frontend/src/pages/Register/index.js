8566d3ebdd5dc940b028192a34506a6915f5cb93
alanams97 <alan_ams97@gmail.com>
2020-03-25 19:51:51 -0300

    Terminada estilização rota Register

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -15,13 +15,24 @@ export default function Register() {
                 <h1>Cadastro</h1>
                 <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
 
-                <Link className="back-link" to="/register">
+                <Link className="back-link" to="/">
                         <FiArrowLeft size={16} color="#E02041" />
-                        Não tenho cadastro
+                        Já tenho cadastro
                     </Link>
             </section>
 
-            <form></form>
+            <form>
+                <input placeholder="Nome da ONG"/>
+                <input type="email" placeholder="E-mail"/>
+                <input placeholder="WhatsApp"/>
+
+                <div className="input-group">
+                    <input placeholder="Cidade"/>
+                    <input placeholder="UF" style={{ width: 80 }}/>
+                </div>
+
+                <button className="button" type="submit">Cadastrar</button>
+            </form>
         </div>
     </div>
     )
