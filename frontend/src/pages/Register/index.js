8d767dd25b5ca912ecc04c3da2d7a2048d22935b
alanams97 <alan_ams97@gmail.com>
2020-03-25 18:58:06 -0300

    Alterado CSS do Link para global

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import './styles.css'
 
+import logoImg from '../../assets/logo.svg';
+
 export default function Register() {
-    return <h1>Register</h1>
+    return(
+    <div className="register-container">
+        <div className="content">
+            <section>
+                <img src={logoImg} alt="Logo - Be The Hero"/>
+                <h1>Cadastro</h1>
+                <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
+
+                
+            </section>
+
+            <form></form>
+        </div>
+    </div>
+    )
 }
