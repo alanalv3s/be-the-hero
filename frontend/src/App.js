ff1687bcd70117238fac0782f45d813e26c19ca7
alanams97 <alan_ams97@gmail.com>
2020-03-25 14:37:18 -0300

    Add counter (teste)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,19 @@ import React from 'react';
 import Header from './Header';
 
 function App() {
+let counter = 0;
+
+function increment (){
+  counter ++;
+}
+
+console.log(counter)
+
   return (
-    <Header>
-      Be The Hero
-    </Header>
+    <div>s
+    <Header>Contador: {counter} </Header>
+      <button onClick={increment}>Incrementar</button>
+      </div>
   );
 }
 
