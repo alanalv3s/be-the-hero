2cdaef43417d9dcbd93e625b7621894afaa65c22
alanams97 <alan_ams97@gmail.com>
2020-03-25 19:13:17 -0300

    Correções

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+
 import './styles.css'
 
 import logoImg from '../../assets/logo.svg';
@@ -12,7 +15,10 @@ export default function Register() {
                 <h1>Cadastro</h1>
                 <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
 
-                
+                <Link className="back-link" to="/register">
+                        <FiArrowLeft size={16} color="#E02041" />
+                        Não tenho cadastro
+                    </Link>
             </section>
 
             <form></form>
