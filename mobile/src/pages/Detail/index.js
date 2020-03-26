b996ae43da7b38d9758d69da0667668281c4d7ae
alanams97 <alan_ams97@gmail.com>
2020-03-26 12:47:12 -0300

    Iniciando Pages

---
+++ mobile/src/pages/Detail/index.js
@@ -0,0 +1,8 @@
+import React from 'React';
+import { View } from 'react-native';
+
+export default function Detail() {
+    return (
+        <View />
+    )
+}
---
+++ mobile/src/pages/Incidents/index.js
@@ -0,0 +1,8 @@
+import React from 'React';
+import { View } from 'react-native';
+
+export default function Incidents() {
+    return (
+        <View />
+    )
+}
